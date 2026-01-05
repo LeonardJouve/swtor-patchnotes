8520a564f79d7b6610ef7fe8d0b9a85c6d22ae0e
Léonard Jouve <leonard.jouve@gmail.com>
2026-01-05 23:53:37 +0100

    add csv headers

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import {getPatchesList, parsePatch} from "./patch";
-import {formatCSV, write} from "./format";
+import {formatCSV, formatCSVHeader, write} from "./format";
 
 getPatchesList()
     .then((list) => Promise.all(list.map(async (patch) => ({
         patch,
         tree: await parsePatch(patch.url),
     }))))
-    .then((result) => result
-        .flatMap(({patch, tree}) => tree.map((node) => formatCSV(patch, node)))
+    .then((result) => [formatCSVHeader()]
+        .concat(result.flatMap(({patch, tree}) => tree.map((node) => formatCSV(patch, node))))
         .join("\n"))
     .then(write("dump.json"));
